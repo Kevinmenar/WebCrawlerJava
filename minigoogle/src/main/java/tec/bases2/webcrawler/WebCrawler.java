6ff5b67ef0a1f9f1f4b65925fd5451ab6fd20f53
Kevinmenar <kevinmenar@gmail.com>
2020-07-10 12:48:32 -0600

    Add web crawler

--- minigoogle/src/main/java/tec/bases2/webcrawler/WebCrawler.java
+++ minigoogle/src/main/java/tec/bases2/webcrawler/WebCrawler.java
@@ -23,15 +23,37 @@ public class WebCrawler {
                 }
 
                 Document document = Jsoup.connect(URL).get();
-                Elements linksOnPage = document.select("a");
+                // System.out.println("Document " + document);
+                System.out.println(htmlTags(document));
+                Elements linksOnPage = document.select("body");
 
                 for (Element page : linksOnPage) {
                 	System.out.println(page.text());
-                    getPageLinks(page.attr("abs:href"));
+                    //getPageLinks(page.attr("abs:href"));
                 }
             } catch (IOException e) {
                 System.err.println("For '" + URL + "': " + e.getMessage());
             }
+        }
+    }
+	
+	private static String htmlTags(Document doc) {
+        StringBuilder sb = new StringBuilder();
+        htmlTags(doc.children(), sb);
+        return sb.toString();
+    }
+
+    private static void htmlTags(Elements elements, StringBuilder sb) {
+        for(Element el:elements) {
+            // System.out.println("el #1: " + el.text().length());
+            // System.out.println("el children size #1: " + el.childrenSize());
+            if(el.text().length() > 0 && el.childrenSize() == 0) {
+            	sb.append(el.nodeName());
+            	sb.append(" / ");
+            	sb.append(el.text());
+            	sb.append(", ");
+            }
+            htmlTags(el.children(), sb);
         }
     }
 }
